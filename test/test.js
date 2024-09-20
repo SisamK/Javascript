fbfdc340142d3f802a36dc9c6e80392fb39a7cc6
SisamK <143947966+SisamK@users.noreply.github.com>
2024-09-20 08:44:40 +0000

    conversion data

---
+++ test/test.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//alert cannot be used in node js
+
+
+//console.log("Hello");
+
+
+let name = "Sisam"
+let age = 4
+let  isVerified= "jjj"
+
+
+//number
+//bigint
+//string =>""
+//boolean= true/false
+//null = standalone value/object
+//undefined => value is not assignment
+//symbol => uniqueness
+
+//object=> 
+ //console.log(typeof"sisam")
+
+let score = "llpp"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let booleanIsVerified = Boolean(isVerified)
+console.log(booleanIsVerified);
+
